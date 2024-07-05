4f06adea3889153b7c4bde57e9d9748cd064df0d
Petruska Ákos <petruskaakos@gmail.com>
2024-07-05 13:28:00 +0200

    chane route

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -35,13 +35,11 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route_table" "internet_rt" {
-
     vpc_id = aws_vpc.main.id
 
     route = {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-        
+        gateway_id = aws_internet_gateway.igw.id       
     }
 
     tags = {
