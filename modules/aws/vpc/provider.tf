c35be07e630b200c473013e6e66424e8d1cae797
Petruska Ákos <petruskaakos@gmail.com>
2024-07-05 13:00:23 +0200

    Fix

--- modules/aws/vpc/provider.tf
+++ modules/aws/vpc/provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
     required_providers {
         aws= {
-            required_version = ">= 2.7.0"
+            version = ">= 2.7.0"
             source = "hashicorp/aws"
         }
       
