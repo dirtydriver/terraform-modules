94002bdb16b7b5b23a79d3dd2946c5f27062b278
Petruska Ákos <petruskaakos@gmail.com>
2024-07-05 13:17:46 +0200

    route table change

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -38,10 +38,10 @@ resource "aws_route_table" "internet_rt" {
 
     vpc_id = aws_vpc.main.id
 
-    route = {
+    route =[ {
         cidr_block= "0.0.0.0/0"
         gateway_id= aws_internet_gateway.igw.id
-    }
+    }]
 
     tags = {
         Name: "Route to InternetGW"
