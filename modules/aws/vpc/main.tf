6f8dde13220b2961f3b9c5c076723e359481e806
Petruska Ákos <petruskaakos@gmail.com>
2024-07-09 23:06:47 +0200

    change route

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -70,18 +70,19 @@ resource "aws_route_table" "internet_rt" {
 
 
 resource "aws_route_table" "private" {
+  count = length(var.public_subnet_cdirs)
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = element(aws_nat_gateway.nat[*].id,count.index)
   }
 }
 
 resource "aws_route_table_association" "private" {
   count = length(var.public_subnet_cdirs)
   subnet_id      = element(aws_subnet.private_subnets[*].id,count.index)
-  route_table_id = aws_route_table.private.id
+  route_table_id = element(aws_route_table.private[*].id,count.index)
 }
 
 resource "aws_route_table_association" "public_subnet_asso" {
