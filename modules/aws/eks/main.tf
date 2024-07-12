fef3f5861dd5301942836b7627f0167168f96da2
Petruska Ákos <petruskaakos@gmail.com>
2024-07-12 12:46:41 +0200

    remove and fix

--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -1,10 +1,9 @@
 resource "aws_eks_cluster" "k8s_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
-  count = length(var.subnet_ids)
 
   vpc_config {
-    subnet_ids = element(var.subnet_ids,count.index)
+    subnet_ids = var.subnet_ids
     vpc_id = var.vpc_id
   }
 
--- modules/aws/eks/outputs.tf
+++ modules/aws/eks/outputs.tf
@@ -1,9 +1,9 @@
 output "endpoint" {
-  value = data.aws_eks_cluster.k8s_cluster.endpoint
+  value = aws_eks_cluster.k8s_cluster.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = data.aws_eks_cluster.k8s_cluster.certificate_authority[0].data
+  value = aws_eks_cluster.k8s_cluster.certificate_authority[0].data
 }
 
 output "node_role_arn" {
