e940dc6ea6707f457abe0c0a6e277bec7e7b60e4
Petruska Ákos <petruskaakos@gmail.com>
2024-07-26 08:10:46 +0200

    fix: fix variable name

--- modules/aws/aws_key_pair/outputs.tf
+++ modules/aws/aws_key_pair/outputs.tf
@@ -1,5 +1,5 @@
 output "private_key_pem" {
-  value     = var.put_priv_in_outputs ? tls_private_key.example.private_key_pem : null
+  value     = var.put_priv_in_outputs ? tls_private_key.priv_key.private_key_pem : null
   sensitive = true
 }
 
