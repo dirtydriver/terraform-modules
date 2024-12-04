17304df88323a44b7aad53640ea084632acbe3f1
Petruska Ákos <petruskaakos@gmail.com>
2024-12-04 22:43:15 +0100

    feat: add kubeconfig template

--- modules/aws/eks/outputs.tf
+++ modules/aws/eks/outputs.tf
@@ -20,3 +20,12 @@ output "cluster_id" {
   description = "EKS Cluster ID"
   value       = aws_eks_cluster.k8s_cluster.id
 }
+
+output "kubeconfig" {
+  description = "Kubeconfig file content to access the EKS cluster"
+  value = templatefile("${path.module}/kubeconfig.tpl", {
+    cluster_name                      = aws_eks_cluster.k8s_cluster.name
+    cluster_endpoint                  = aws_eks_cluster.k8s_cluster.endpoint
+    cluster_certificate_authority_data = aws_eks_cluster.k8s_cluster.certificate_authority[0].data
+  })
+}
